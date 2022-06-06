2d3a008e83e1671621abfb83f41505058a86b8bc
Rodrigo Cauê Füster <rdgfuster@gmail.com>
2022-06-06 19:01:50 -0300

    exercicios
    exercícios realizados no dia 06/06/22

---
+++ fundamentos/exercicios/math.js
@@ -0,0 +1,13 @@
+const raio = 5.6 
+const area = Math.PI * Math.pow(raio, 2) 
+
+console.log(area) 
+console.log(typeof Math)
+
+var num = Math.sign(20 - 22)
+console.log(num)
+var nun2 = Math.pow(2, 3)
+console.log(nun2) 
+
+var  raiz = Math.sqrt(9)
+console.log(raiz)
---
+++ fundamentos/exercicios/numeros.cuidados.js
@@ -0,0 +1,9 @@
+console.log(7 / 0); // divisão fica infinita
+
+console.log("11" / 2);
+
+console.log("show!" * 2); // NaN = não é número.
+
+console.log(0.1 + 0.7);
+
+console.log((10.257).toFixed(2));
