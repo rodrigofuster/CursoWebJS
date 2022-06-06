a1637e3ca4f88c14045a13c49e17f89e02b10273
Rodrigo Cauê Füster <rdgfuster@gmail.com>
2022-06-06 20:13:44 -0300

    atualizacao.aulas
    exercícios realizados no dia 06/06/22

--- fundamentos/exercicios/strings.js
+++ fundamentos/exercicios/strings.js
@@ -1,3 +1,18 @@
-const escola = "Cod3r"
-console.log(escola.charAt(4))
-console.log(escola.charCodeAt(3))
+const escola = "Cod3r é um bom curso";
+console.log(escola.charAt(4));
+console.log(escola.charCodeAt(3));
+
+console.log(escola.indexOf("é"));
+console.log(escola.lastIndexOf("o"));
+
+console.log(escola.substring(1));
+console.log(escola.substring(0, 3));
+
+console.log(escola.replace(3, "e"));
+
+let str = "Apple, Banana, Kiwi";
+let part = str.slice(7, 13);
+console.log(part);
+
+//arrays
+console.log("Ana,Maria,Pedro".split(","));
