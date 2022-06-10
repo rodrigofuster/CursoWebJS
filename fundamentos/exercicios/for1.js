a4656723056f60036c45be4064d0719fda2f468f
Rodrigo Cauê Füster <rdgfuster@gmail.com>
2022-06-10 15:56:30 -0300

    Update for1.js

--- fundamentos/exercicios/for1.js
+++ fundamentos/exercicios/for1.js
@@ -2,9 +2,8 @@ let contador = 0;
 while (contador <= 10) {
   console.log("contador = " + contador);
   contador++;
-} 
+}
 
-let contador2 = "";
 for (let contador2 = 0; contador2 <= 10; contador2++) {
-  console.log(" contador 2 = " + contador2);
+  console.log(`contador 2 =  ${contador2}`);
 }
