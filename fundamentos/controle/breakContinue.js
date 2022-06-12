c3122f161e7f0bef3e7c9dfa433bfd7520c11323
Rodrigo Cauê Füster <rdgfuster@gmail.com>
2022-06-12 14:35:55 -0300

    Update breakContinue.js

--- fundamentos/controle/breakContinue.js
+++ fundamentos/controle/breakContinue.js
@@ -4,5 +4,21 @@ for (x in nums) {
   if (x == 5) {
     break;
   }
-  console.log();
+  console.log(`${x} = ${nums[x]}`);
 }
+
+for (y in nums) {
+  if (y == 5) {
+    continue;
+  }
+  console.log(`${y} = ${nums[y]}`);
+}
+
+externo: for (a in nums) {
+  for (b in nums) {
+    if (a == 2 && b == 3) break externo;
+    console.log(`Par = ${a}, ${b}`);
+  }
+}
+
+console.log(`Fim!`);
