48cf3ec2f6266fbb265849df6557c99112816ccd
Rodrigo Cauê Füster <rdgfuster@gmail.com>
2022-06-12 15:26:45 -0300

    Update cidadaoPrimeiraClasse.js

--- fundamentos/funções/cidadaoPrimeiraClasse.js
+++ fundamentos/funções/cidadaoPrimeiraClasse.js
@@ -1,1 +1,39 @@
-// Função
+// Função em JS é Fisrt-Class Object (Citizens)
+// Higher-order function
+
+// criar de forma literal
+function fun1() {}
+
+// Armazenar dentro de uma variável
+const fun2 = function () {};
+
+// Armazenar dentro de um array
+const array = [
+  function (a, b) {
+    return a + b;
+  },
+];
+console.log(array[0](2, 3));
+
+// Armazenar em um atributo de objeto
+const obj = {};
+obj.falar = function () {
+  return "Opa";
+};
+console.log(obj.falar());
+
+// Passar função como parametro
+function run(fun) {
+  fun();
+}
+run(function () {
+  console.log(`Executando...`);
+});
+
+// Uma função pode retornar/conter uma função
+function soma(a, b) {
+  return function (c) {
+    console.log(a + b + c);
+  };
+}
+soma(2, 3)(4);
