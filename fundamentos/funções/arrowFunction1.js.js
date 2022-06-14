89e343a9d199fb5f5b9e6c9f49994abf8fbbdb6b
Rodrigo Cauê Füster <rdgfuster@gmail.com>
2022-06-14 14:33:47 -0300

    Create arrowFunction1.js.js

---
+++ fundamentos/funções/arrowFunction1.js.js
@@ -0,0 +1,18 @@
+let dobro = function (a) {
+    return 2 * a
+}
+
+dobro = (a) => { //arrow function
+    return 2 * a
+}
+
+dobro = a => 2 * a // return implícito
+console.log(dobro(4));
+
+let ola = function () {
+    return `Olá`
+}
+
+ola = () => `Olá`
+
+console.log(ola() + ` Carlos.`);
