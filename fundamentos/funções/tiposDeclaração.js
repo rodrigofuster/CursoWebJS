63f0dafa085caf8bdeb0511a28852ae16ea6ecb5
Rodrigo Cauê Füster <rdgfuster@gmail.com>
2022-06-15 14:29:58 -0300

    add-aulas

---
+++ fundamentos/funções/tiposDeclaração.js
@@ -0,0 +1,14 @@
+// function delaration
+function soma(x, y) {
+  return x + y;
+}
+
+// function espression - anônima
+const sub = function (x, y) {
+  return x - y;
+};
+
+// named function expression
+const mult = function mult(x, y) {
+  return x * y;
+};
