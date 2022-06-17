acff3d9b575fa5e4b02f4970d76e72a716bf3076
Rodrigo Cauê Füster <rdgfuster@gmail.com>
2022-06-17 09:39:48 -0300

    Create oo.js

---
+++ fundamentos/objetos/oo.js
@@ -0,0 +1,22 @@
+// CÓDIGO NÃO EXECUTÁVEL!!
+
+// Procedural
+processamento(valor1, valor2, valor3);
+
+// OO
+objeto = {
+  valor1,
+  valor2,
+  valor3,
+  processamento() {
+    //...
+  },
+};
+
+objeto.processamento(); // o foco passou a ser o objeto.
+
+// princípios importantes
+// 1. Abstração
+// 2. encapsulamento
+// 3. herança (prototype)
+// 4. Polimorfismo 
