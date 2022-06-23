a4469e62d09b591033321bba427b60c8376f1b46
Rodrigo Cauê Füster <rdgfuster@gmail.com>
2022-06-23 14:47:06 -0300

    att-aulas

--- fundamentos/array/arrayMetodos.js
+++ fundamentos/array/arrayMetodos.js
@@ -6,10 +6,25 @@ console.log(pilotos);
 pilotos.push("Verstappen"); // adiciona um elemeto no final
 console.log(pilotos);
 
-pilotos.shift(); // remove o primeiro elemento 
+pilotos.shift(); // remove o primeiro elemento
 console.log(pilotos);
 
-pilotos.unshift('Hamilton') // adiciona o primeiro elemento 
+pilotos.unshift("Hamilton"); // adiciona o primeiro elemento
 console.log(pilotos);
 
-// adici
+// splice pode adicionar e remover elementos
+
+// adicionar
+pilotos.splice(2, 0, "Botas", "Massa");
+console.log(pilotos);
+
+// remover 
+pilotos.splice(3,1) 
+console.log(pilotos);
+
+// novo array 
+const algunsPilotos1 = pilotos.slice(2)
+console.log(algunsPilotos1);
+
+const algunsPilotos2 = pilotos.slice(1,4)
+console.log(algunsPilotos2);
