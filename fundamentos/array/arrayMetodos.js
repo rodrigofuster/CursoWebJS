543572b685a1d2c30c86607084b17c8fece6b281
Rodrigo Cauê Füster <rdgfuster@gmail.com>
2022-06-23 14:29:24 -0300

    Update arrayMetodos.js

--- fundamentos/array/arrayMetodos.js
+++ fundamentos/array/arrayMetodos.js
@@ -1,1 +1,15 @@
 const pilotos = ["Vettel", "Alonso", "Raikkonen", "Massa"];
+console.log(pilotos);
+pilotos.pop(); // remove o ultimo elemento
+console.log(pilotos);
+
+pilotos.push("Verstappen"); // adiciona um elemeto no final
+console.log(pilotos);
+
+pilotos.shift(); // remove o primeiro elemento 
+console.log(pilotos);
+
+pilotos.unshift('Hamilton') // adiciona o primeiro elemento 
+console.log(pilotos);
+
+// adici
