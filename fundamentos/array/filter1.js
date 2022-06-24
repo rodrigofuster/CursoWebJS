a2a54a5829cc7adab2370c41c98b0f323c2676f9
Rodrigo Cauê Füster <rdgfuster@gmail.com>
2022-06-24 14:07:52 -0300

    Create filter1.js

---
+++ fundamentos/array/filter1.js
@@ -0,0 +1,6 @@
+const produtos = [
+    {nome: 'Note', preco: 2499, fragil: true}
+    {nome: 'Ipad Pro', preco: 4199, fragil: true}
+    {nome: "Cpode Vidro", preco: 18.99, fragil: true}
+    {nome: "Cpode de Plástico", preco: 12.49, fragil: false}
+]
