60e5c464bff994d2183279cc5fc66cce8fdb105c
Rodrigo Cauê Füster <rdgfuster@gmail.com>
2022-06-24 14:20:38 -0300

    Update filter1.js

--- fundamentos/array/filter1.js
+++ fundamentos/array/filter1.js
@@ -1,6 +1,15 @@
 const produtos = [
-    {nome: 'Note', preco: 2499, fragil: true}
-    {nome: 'Ipad Pro', preco: 4199, fragil: true}
-    {nome: "Cpode Vidro", preco: 18.99, fragil: true}
+    {nome: 'Note', preco: 2499, fragil: true},
+    {nome: 'Ipad Pro', preco: 4199, fragil: true},
+    {nome: "Cpode Vidro", preco: 18.99, fragil: true},
     {nome: "Cpode de Plástico", preco: 12.49, fragil: false}
 ]
+
+console.log(produtos.filter(function(p) {
+    return p.produtos < 2400
+}))
+
+const caro = produto => produto.preco >= 500 
+const fragil = produto => produto.fragil
+
+console.log(produtos.filter(caro).filter(fragil));
